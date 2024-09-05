e5dfb9a3264e42476c390e6d5d7bceb700ccd0c0
ajian_nedo <asianerd28@gmail.com>
2024-09-05 15:20:14 +0800

    Project deletion, creation and editing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -23,11 +23,19 @@ async fn rocket() -> _ {
 
         .mount("/project/fetch/all", routes![project::fetch_all])
         .mount("/project/fetch/owned", routes![project::fetch_own_projects])
-        .mount("/project/fetch/members", routes![project::fetch_project_members])
+
+        .mount("/project/member/fetch", routes![project::fetch_project_members])
+        .mount("/project/member/add", routes![project_members::add])
+        .mount("/project/member/remove", routes![project_members::remove])
+
+        .mount("/project/create", routes![project::create])
+        .mount("/project/delete", routes![project::delete])
+        .mount("/project/edit", routes![project::edit])
         
         .mount("/task/fetch", routes![task::fetch_tasks])
         .mount("/task/create", routes![task::create])
         .mount("/task/delete", routes![task::delete])
+        .mount("/task/edit", routes![task::edit])
 
         .mount("/user/ensure_existance", routes![user::ensure_existance])
 }
--- backend/src/project.rs
+++ backend/src/project.rs
@@ -16,6 +16,33 @@ impl Project {
             .await
             .unwrap()
     }
+
+    pub async fn create(db: &Pool<Sqlite>, title: String, description: String) {
+        sqlx::query("insert into project(title, description) values($1, $2);")
+            .bind(title)
+            .bind(description)
+            .execute(db)
+            .await
+            .unwrap();
+    }
+
+    pub async fn delete(db: &Pool<Sqlite>, project_id: i64) {
+        sqlx::query("delete from project where id = $1;")
+            .bind(project_id)
+            .execute(db)
+            .await
+            .unwrap();
+    }
+
+    pub async fn edit(db: &Pool<Sqlite>, project_id: i64, title: String, description: String) {
+        sqlx::query("update project set title = $1, description = $2 where id = $3")
+            .bind(title)
+            .bind(description)
+            .bind(project_id)
+            .execute(db)
+            .await
+            .unwrap();
+    }
 }
 
 #[get("/")]
@@ -34,9 +61,72 @@ pub async fn fetch_own_projects(db: &State<Pool<Sqlite>>, login: LoginInformatio
     }
 }
 
-#[get("/<project_id>")]
-pub async fn fetch_project_members(db: &State<Pool<Sqlite>>, project_id: i64) -> String {
-    utils::parse_response(Ok(ProjectMembers::fetch_members(db.inner(), project_id).await))
+#[post("/<project_id>", data="<login>")]
+pub async fn fetch_project_members(db: &State<Pool<Sqlite>>, login: LoginInformation, project_id: i64) -> String {
+    let db = db.inner();
+    let result = login.login(&db).await;
+    match result {
+        LoginResult::Success(u) => {
+            if ProjectMembers::is_member(db, u, project_id).await {
+                return utils::parse_response(Ok(ProjectMembers::fetch_members(db, project_id).await));
+            }
+            utils::parse_response(Ok("not a member of project"))
+        },
+        _ => {
+            utils::parse_response(Err(result))
+        }
+    }
 }
 
+#[post("/<title>/<description>", data="<login>")]
+pub async fn create(db: &State<Pool<Sqlite>>, login: LoginInformation, title: String, description: String) -> String {
+    let db = db.inner();
+    let result = login.login(&db).await;
+    match result {
+        LoginResult::Success(_) => {
+            Project::create(db, title, description).await;
 
+            utils::parse_response(Ok("created"))
+        },
+        _ => {
+            utils::parse_response(Err(result))
+        }
+    }
+}
+
+#[post("/<project_id>", data="<login>")]
+pub async fn delete(db: &State<Pool<Sqlite>>, login: LoginInformation, project_id: i64) -> String {
+    let db = db.inner();
+    let result = login.login(&db).await;
+    match result {
+        LoginResult::Success(u) => {
+            if ProjectMembers::is_member(db, u, project_id).await {
+                Project::delete(db, project_id).await;
+
+                return utils::parse_response(Ok("deleted"));
+            }
+            utils::parse_response(Ok("not a member of project"))
+        },
+        _ => {
+            utils::parse_response(Err(result))
+        }
+    }
+}
+
+#[post("/<project_id>/<title>/<description>", data="<login>")]
+pub async fn edit(db: &State<Pool<Sqlite>>, login: LoginInformation, project_id: i64, title: String, description: String) -> String {
+    let db = db.inner();
+    let result = login.login(&db).await;
+    match result {
+        LoginResult::Success(u) => {
+            if ProjectMembers::is_member(db, u, project_id).await {
+                Project::edit(db, project_id, title, description).await;
+                return utils::parse_response(Ok("edited"));
+            }
+            utils::parse_response(Ok("not a member of project"))
+        },
+        _ => {
+            utils::parse_response(Err(result))
+        }
+    }
+}
