b2b3f6a0bc455a75a3b404801c0d4c81387c49de
ajian_nedo <asianerd28@gmail.com>
2024-09-05 15:20:28 +0800

    Project members adding and removing

--- backend/src/project_members.rs
+++ backend/src/project_members.rs
@@ -1,7 +1,8 @@
+use rocket::State;
 use serde::{Deserialize, Serialize};
 use sqlx::{prelude::FromRow, Pool, Sqlite};
 
-use crate::{project::Project, user::User, utils::ValueInt};
+use crate::{login_info::{LoginInformation, LoginResult}, project::Project, user::User, utils::{self, ValueInt}};
 
 #[derive(FromRow, Serialize, Deserialize)]
 pub struct ProjectMembers {
@@ -33,5 +34,61 @@ impl ProjectMembers {
             .fetch_one(db)
             .await
             .unwrap().0 >= 1
+    }
+
+    pub async fn add(db: &Pool<Sqlite>, user_id: i64, project_id: i64) {
+        sqlx::query("insert into project_members(project_id, user_id) values($1, $2);")
+            .bind(project_id)
+            .bind(user_id)
+            .execute(db)
+            .await
+            .unwrap();
+    }
+
+    pub async fn remove(db: &Pool<Sqlite>, user_id: i64, project_id: i64) {
+        sqlx::query("delete from project_members where user_id = $1, project_id = $2;")
+            .bind(user_id)
+            .bind(project_id)
+            .execute(db)
+            .await
+            .unwrap();
+    }
+}
+
+#[post("/<project_id>/<user_id>", data="<login>")]
+pub async fn add(db: &State<Pool<Sqlite>>, login: LoginInformation, project_id: i64, user_id: i64) -> String {
+    let db = db.inner();
+    let result = login.login(&db).await;
+    match result {
+        LoginResult::Success(u) => {
+            if ProjectMembers::is_member(db, u, project_id).await {
+                ProjectMembers::add(db, user_id, project_id).await;
+
+                return utils::parse_response(Ok("added"));
+            }
+            utils::parse_response(Ok("not a member of project"))
+        },
+        _ => {
+            utils::parse_response(Err(result))
+        }
+    }
+}
+
+#[post("/<project_id>/<user_id>", data="<login>")]
+pub async fn remove(db: &State<Pool<Sqlite>>, login: LoginInformation, project_id: i64, user_id: i64) -> String {
+    let db = db.inner();
+    let result = login.login(&db).await;
+    match result {
+        LoginResult::Success(u) => {
+            if ProjectMembers::is_member(db, u, project_id).await {
+                ProjectMembers::remove(db, user_id, project_id).await;
+
+                return utils::parse_response(Ok("removed"));
+            }
+            utils::parse_response(Ok("not a member of project"))
+        },
+        _ => {
+            utils::parse_response(Err(result))
+        }
     }
 }
